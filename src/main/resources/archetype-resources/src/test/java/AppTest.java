7b61d61ef7fecc7490d009e5bb9b422add2ae6f8
Vitaliy Boyarsky <boyarsky.vitaliy@live.com>
2017-02-27 00:35:36 +0200

    Initial commit

---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,13 @@
+package ${package};
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        Assert.assertTrue(true);
+    }
+
+}
